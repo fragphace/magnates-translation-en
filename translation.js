50d1f5f2838c99d8b491459671eaa81da6084c4c
fragphace <fragphace@gmail.com>
2015-01-12 09:31:38 +0100

    Init translations file

---
+++ translation.js
@@ -0,0 +1,26 @@
+module.exports = function () {
+	return [{
+		pl: {
+			name: 'Prymas',
+			desc: 'Rozstrzygasz remisy w Senacie i Sejmie, w trakcie Konfliktów i w Interregnum.'
+		},
+		en: {}
+	}, {
+		pl: {
+			name: 'Podskarbi',
+			desc: 'Zagraj w Interregnum. Zamień dowolny 1D w Prowincji, w której nie ma R, na własny.'
+		},
+		en: {}
+	}, {
+		pl: {
+			name: 'Hetman',
+			desc: 'Zagraj w chwili rozstrzygania Konfliktu. Siłę zagranej przez siebie karty Familii zwiększasz o +3.'
+		},
+		en: {}
+	}, {
+		pl: {
+			name: 'Kanclerz',
+			desc: 'Zagraj w chwili rozpatrywania karty Praw. Wpływy zagranej przez siebie karty Familii zwiększasz o +3.'
+		}
+	}];
+};
